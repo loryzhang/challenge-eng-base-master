11349b8011026a9a0bab872ddc45643da9cd5e82
Lory Zhang <zhang.rouyun@gmail.com>
2018-03-22 23:05:33 -0700

    tried to add nginx

--- backend-node/router.js
+++ backend-node/router.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const auth = require('./auth');
 
-// router.get('/test', controller.test);
-// router.get('/messages', controller.fetch);
-// router.post('/message', controller.send);
 router.get('/session', auth.checkSession);
 router.post('/login', auth.login);
 router.get('/logout', auth.logout);
